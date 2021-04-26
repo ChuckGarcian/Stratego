7cdc5208cdd2ee5cfa56ba2b6a8ee07130c1ebee
ChuckGarcian <chuckgarcian@gmail.com>
2021-04-26 09:31:12 -0500

    Version - 1

--- Stratego.java
+++ Stratego.java
@@ -1,34 +1,16 @@
 
 /**
- * Write a description of class Stratego here.
+ * Stratego!
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Chuck Garcia 
+ * @version Version-1 Apr-26-21
  */
 public class Stratego
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Stratego
-     */
     public Stratego()
     {
-        // initialise instance variables
-                //made change
+     System.out.println("Hello World");
         
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
